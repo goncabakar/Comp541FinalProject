3fd0af61ccd57830f331b6f42bca8cd8a283d9d9
goncabakar <gbakar15@ku.edu.tr>
2017-05-01 12:34:28 +0300

    Update mnist_updated.jl

--- mnist_updated.jl
+++ mnist_updated.jl
@@ -1,6 +1,6 @@
 
 for p in ("Knet", "Compat", "GZip", "Images")
-    Pkg.installed(p) == nothing && Pkg.add(p)
+  Pkg.installed(p) == nothing && Pkg.add(p)
 end
 
 using Knet, Compat, GZip, Images
@@ -17,60 +17,58 @@ function main()
   w_disc = weights_discriminator()
   opts_disc = map(x->Adam(lr=0.0002, beta1=0.5), w_disc)
   opts_gen = map(x->Adam(lr=0.0002, beta1=0.5), w_gen)
-
+  
   noise = randn(Float32,100,batchsize)
   #noise = convert(KnetArray{Float32}, noise)
   fake_image = generator(w_gen, noise,mode=0)
   loss_gen = loss_generator(w_gen, w_disc, noise)
   loss_disc = loss_discriminator(w_disc, xtrn_mb[1], fake_image)
-
+  
   report(0,loss_gen,loss_disc,accuracy(w_gen, w_disc, xtst,0))
-
-  @time for epoch=1:10
-        train(w_gen, w_disc, xtrn_mb, epoch, xtst, opts_disc, opts_gen)
+  
+  @time for epoch=1:20
+    train(w_gen, w_disc, xtrn_mb, epoch, xtst, opts_disc, opts_gen)
   end
-
+  
 end
 
 report(epoch, lossgen, lossdisc, accuracy)=println((:epoch,epoch,:generatorloss,lossgen,:discriminatorloss,lossdisc,:accuracy_real_fake,accuracy))
 
 function load_data()
-    a = gzload("train-images-idx3-ubyte.gz")[17:end]
-    b = gzload("t10k-images-idx3-ubyte.gz")[17:end]
-    xtrn = convert(Array{Float32}, reshape(2*((a ./ 255)-0.5), 28*28, div(length(a), 784)))
-    xtst = convert(Array{Float32}, reshape(2*((b ./ 255)-0.5), 28*28, div(length(b), 784)))
-    #xtrn = convert(Array{Float32}, reshape((a ./ 255), 28*28, div(length(a), 784)))
-    #xtst = convert(Array{Float32}, reshape((b ./ 255), 28*28, div(length(b), 784)))
-
-    return xtrn, xtst
+  a = gzload("train-images-idx3-ubyte.gz")[17:end]
+  b = gzload("t10k-images-idx3-ubyte.gz")[17:end]
+  xtrn = convert(Array{Float32}, reshape(2*((a ./ 255)-0.5), 28*28, div(length(a), 784)))
+  xtst = convert(Array{Float32}, reshape(2*((b ./ 255)-0.5), 28*28, div(length(b), 784)))
+  
+  return xtrn, xtst
 end
 
 function gzload(file; path="$file", url="http://yann.lecun.com/exdb/mnist/$file")
-	isfile(path) || download(url, path)
-	f = gzopen(path)
-	a = @compat read(f)
-	close(f)
-	return(a)
+  isfile(path) || download(url, path)
+  f = gzopen(path)
+  a = @compat read(f)
+  close(f)
+  return(a)
 end
 
 function minibatch(X, bs)
-
-    data = Any[]
-    x_mb = Any[]
-    for i=1:round(Int,size(X,2)/bs)-1
+  
+  data = Any[]
+  x_mb = Any[]
+  for i=1:round(Int,size(X,2)/bs)-1
     x_mb=X[1:784,((i-1)*bs)+1 : i*bs]
     push!(data, x_mb)
-    end
-
-    return data
+  end
+  
+  return data
 end
 
 function minibatch4(x, batchsize)
-    data = minibatch(x, batchsize)
-    for i=1:length(data)
-        data[i] = (reshape(data[i], (28,28,1,batchsize)))
-    end
-    return data
+  data = minibatch(x, batchsize)
+  for i=1:length(data)
+    data[i] = (reshape(data[i], (28,28,1,batchsize)))
+  end
+  return data
 end
 
 function generator(w, noise; mode=0)
@@ -85,21 +83,19 @@ function generator(w, noise; mode=0)
   x = relu(x);
   x = batchnorm(deconv4(w[5],x;padding=2, stride=2) .+ w[6]);
   x = relu(x);
-  #x = batchnorm(deconv4(w[7],x;padding=2, stride=2) .+ w[8]);
   x = (deconv4(w[7],x;padding=2, stride=2) .+ w[8]);
-
-if mode == 1
-  fake_images = sigm(x);
-else
-  fake_images = tanh(x);
-end
+  
+  if mode == 1
+    fake_images = sigm(x);
+  else
+    fake_images = tanh(x);
+  end
   return fake_images
 end
 
 function discriminator(w, x)
   #decides whether image is fake or real
   #inputs can be generated images or real images from dataset
-  #x = batchnorm(conv4(w[1],x;padding=2, stride=2) .+ w[2])
   x = (conv4(w[1],x;padding=2, stride=2) .+ w[2])
   x = leaky_relu(x)
   x = batchnorm(conv4(w[3],x;padding=2, stride=2) .+ w[4])
@@ -109,12 +105,12 @@ function discriminator(w, x)
   x = mat(x)
   x = w[7]*x .+ w[8]
   x = sigm(x)
-
+  
   return x
 end
 
 function leaky_relu(x)
-return  max(0.2*x,x)
+  return  max(0.2*x,x)
 end
 
 function weights_discriminator()
@@ -151,121 +147,105 @@ function weights_generator()
 end
 
 function loss_discriminator(w_disc, real_image, fake_image)
-#use for update discriminator's weights
-epsilon=1e-10
-#epsilon=0
-ypred_real = discriminator(w_disc,real_image)
-ynorm_real = log(ypred_real+epsilon)
-J_real = -sum(ynorm_real) / size(ynorm_real,2)
-ypred_fake = discriminator(w_disc,fake_image)
-ynorm_fake = log(1-ypred_fake+epsilon)
-J_fake = -sum(ynorm_fake) / size(ynorm_fake,2)
-return 0.5*J_real + 0.5*J_fake
+  #use for update discriminator's weights
+  epsilon=1e-10
+  #epsilon=0
+  ypred_real = discriminator(w_disc,real_image)
+  ynorm_real = log(ypred_real+epsilon)
+  J_real = -sum(ynorm_real) / size(ynorm_real,2)
+  ypred_fake = discriminator(w_disc,fake_image)
+  ynorm_fake = log(1-ypred_fake+epsilon)
+  J_fake = -sum(ynorm_fake) / size(ynorm_fake,2)
+  return 0.5*J_real + 0.5*J_fake
 end
 
 function loss_generator(w_gen, w_disc, noise)
-#use for update generator's weights
-epsilon=1e-10
-#epsilon=0
-fake_image = generator(w_gen, noise;mode=0)
-ypred = discriminator(w_disc,fake_image)
-ynorm_fake = log(ypred+epsilon)
-J = -sum(ynorm_fake) / size(ypred,2)
-return J
+  #use for update generator's weights
+  epsilon=1e-10
+  #epsilon=0
+  fake_image = generator(w_gen, noise;mode=0)
+  ypred = discriminator(w_disc,fake_image)
+  ynorm_fake = log(ypred+epsilon)
+  J = -sum(ynorm_fake) / size(ypred,2)
+  return J
 end
 
 grad_gen = grad(loss_generator)
 grad_disc = grad(loss_discriminator)
 
 function accuracy(w_gen, w_disc, xtst, epoch)
-    ncorrect1 = 0
-    ncorrect2 = 0
-    ninstance = 0
-    ninstance2 = 0
-    expected_sum1 = 0
-    expected_sum2 = 0
-    expected_sum = 0
-    for i=1:1
-    #xtst[i] = 2*(xtst[i].-0.5)
+  ncorrect1 = 0
+  ncorrect2 = 0
+  ninstance = 0
+  ninstance2 = 0
+  expected_sum1 = 0
+  expected_sum2 = 0
+  expected_sum = 0
+  for i=1:20
+    
     ypred = discriminator(w_disc, xtst[i])
     expected_sum1 += sum(ypred)
-    #ypred = logp(ypred, 1)
-    #image = xtst[i];
-    #open("test2.raw", "w");
-    #image = round(Int, image.*255);
-    #image = convert(Array{UInt8}, image);
-    #write("test2.raw", image[:,:,1,1])
     ncorrect1 += sum(ypred[1:1,:] .> 0.5)
     ninstance += size(ypred,2)
     noise = randn(Float32,100,128)
     fake_image2 = generator(w_gen, noise;mode=1)
     fake_image = generator(w_gen, noise;mode=0)
-    #open("test_epoch$(epoch)_$(i).raw", "a+")
-    #png = round(Int, fake_image2[:,:,:,1].*255);
-    #png = makegrid(fake_image2; gridsize=[8,8], scale=2.0)
-    #fake_image2 = convert(Array{Float64}, fake_image2)
-    #fake_image2 = round(Int, fake_image2[:,:,:,1].*255);
-    #fake_image2 = convert(Array{UInt8}, fake_image2);
-    #png = convert(Image, fake_image2)
-    #png = colorim(fake_image2)
-
-
     filename = "test_mnist_epoch$(epoch)_$(i).png"
     fake_image2 = reshape(fake_image2[:,:,:,1], 28, 28)
     fake_image2 = colorview(Gray, fake_image2');
     save(filename, fake_image2)
-
-    #fake_image1 = convert(Array{UInt8}, fake_image1);
-    #write("test_epoch$(epoch)_$(i).raw", fake_image1[:,:,1,1])
+    
+    for w in 1:length(w_gen)
+      writedlm(filename*"_weight$w.txt", w_gen[w])
+    end
+    writedlm(filename*"_noise.txt", noise)
+    
     ypred2 = discriminator(w_disc,fake_image)
     expected_sum2 += sum(ypred2)
     ncorrect2 += sum((ypred2[1:1,:]) .< 0.5)
     ninstance2 += size(ypred2,2)
-    end
-    return (ncorrect1/ninstance), (ncorrect2/ninstance2), (expected_sum1+expected_sum2)/(ninstance+ninstance2), (expected_sum1)/(ninstance), (expected_sum2)/(ninstance2)
+  end
+  return (ncorrect1/ninstance), (ncorrect2/ninstance2), (expected_sum1+expected_sum2)/(ninstance+ninstance2), (expected_sum1)/(ninstance), (expected_sum2)/(ninstance2)
 end
 
 function batchnorm(x;epsilon=1e-5)
-          mu, sigma = nothing, nothing
-          d = ndims(x) == 4 ? (1,2,4) : (2,)
-          s = prod(size(x)[[d...]])
-          mu = sum(x,d) / s
-          sigma = sqrt(epsilon + (sum((x.-mu).^2, d)) / s)
-
-    xhat = (x.-mu) ./ sigma
-    return xhat
+  mu, sigma = nothing, nothing
+  d = ndims(x) == 4 ? (1,2,4) : (2,)
+  s = prod(size(x)[[d...]])
+  mu = sum(x,d) / s
+  sigma = sqrt(epsilon + (sum((x.-mu).^2, d)) / s)
+  
+  xhat = (x.-mu) ./ sigma
+  return xhat
 end
 
 function train(w_gen, w_disc, xtrn, epoch, xtst, opts_disc, opts_gen)
   loss_gen = Any[]
   loss_disc = Any[]
   fake_image = Any[]
-
-  #train_bs = 10
+  
+  #train_bs = 100
   train_bs = round(Int,60000/128)-1
   for mb=1:train_bs
+    noise = randn(Float32,100,128)
+    fake_image = generator(w_gen, noise; mode=0)
+    gradient_gen = grad_gen(w_gen, w_disc, noise)
+    gradient_disc = grad_disc(w_disc, xtrn[mb], fake_image)
+    
+    for i in 1:length(w_disc)
+      update!(w_disc[i], gradient_disc[i], opts_disc[i])
+    end
+    for i in 1:length(w_gen)
+      update!(w_gen[i], gradient_gen[i], opts_gen[i])
+    end
+  end
+  
   noise = randn(Float32,100,128)
-  fake_image = generator(w_gen, noise; mode=0)
-  gradient_gen = grad_gen(w_gen, w_disc, noise)
-  gradient_disc = grad_disc(w_disc, xtrn[mb], fake_image)
-
-#if epoch < 5
-  for i in 1:length(w_disc)
-    update!(w_disc, gradient_disc, opts_disc)
-  end
-#else
-  for i in 1:length(w_gen)
-    update!(w_gen, gradient_gen, opts_gen)
-  end
-#end
-end
-
-noise = randn(Float32,100,128)
-loss_gen = loss_generator(w_gen, w_disc, noise)
-loss_disc = loss_discriminator(w_disc, xtrn[train_bs], fake_image)
-report(epoch, loss_gen, loss_disc, accuracy(w_gen, w_disc, xtst, epoch))
-
-return w_gen, w_disc
+  loss_gen = loss_generator(w_gen, w_disc, noise)
+  loss_disc = loss_discriminator(w_disc, xtrn[train_bs], fake_image)
+  report(epoch, loss_gen, loss_disc, accuracy(w_gen, w_disc, xtst, epoch))
+  
+  return w_gen, w_disc
 end
 
 main()
